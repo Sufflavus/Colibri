df3916f61bf7c28abb4c1199ffa5c68ee32f9acd
Sufflavus <tatashat@gmail.com>
2015-10-08 22:11:58 +0300

    * Jasmine tests

---
+++ src/colibri.js
@@ -0,0 +1,139 @@
+(function(){
+
+	if (!Array.prototype.last) {
+		Array.prototype.last = function() {
+		    return this[this.length - 1];
+		};
+	}
+
+	if (!Array.prototype.first) {
+		Array.prototype.first = function() {
+		    return this[0];
+		};
+	}
+
+	if (!Array.prototype.isEmpty) {
+		Array.prototype.isEmpty = function() {
+		    return this.length === 0;
+		};
+	}
+
+	if (!Object.prototype.clone) {
+		Object.prototype.clone = function() {
+			return JSON.parse(JSON.stringify(this));
+		};
+	}
+	
+	if (!Object.prototype.isArray) {
+		Object.prototype.isArray = function() {
+			return Object.prototype.toString.call(this) === "[object Array]";
+		};
+	}
+
+	function isArray(value) {
+	    return Object.prototype.toString.call(value) === "[object Array]";
+	}
+
+	if (!Array.prototype.removeEmpty) {
+	    Array.prototype.removeEmpty = function() {
+	        return this.filter(function(item) { 
+	        	return item; 
+	        });	        
+	    };
+	}
+
+	if (!Array.prototype.contains) {
+	    Array.prototype.contains = function (value) {
+	        return this.indexOf(value) >= 0;
+	    };
+	}
+
+	if (!Array.prototype.indexOfInsensitive) {
+	    Array.prototype.indexOfInsensitive = function (value) {
+	        var upperCaseArray = this.map(function (value) {
+	            return value.toUpperCase();
+	        });
+	        return upperCaseArray.indexOf(value.toUpperCase());
+	    };
+	}
+
+	if (!Array.prototype.max) {
+	    Array.prototype.max = function () {
+	        return Math.max.apply(Math, this);
+	    };
+	}
+
+	if (!Array.prototype.min) {
+	    Array.prototype.min = function () {
+	        return Math.min.apply(Math, this);
+	    };
+	}
+
+	if (!String.prototype.trim) {
+	    String.prototype.trim = function () {
+	        return this.replace(/^\s+|\s+$/g, '');
+	    };
+	}
+
+	if (!String.prototype.indexOfInsensitive) {
+	    String.prototype.indexOfInsensitive = function (searchValue, fromIndex) {
+	        return this.toUpperCase().indexOf(searchValue.toUpperCase(), fromIndex);
+	    };
+	}
+
+	if (!String.prototype.endsWith) {
+	    String.prototype.endsWith = function(suffix) {
+	        return this.indexOf(suffix, this.length - suffix.length) !== -1;
+	    };
+	}
+
+	if (!String.prototype.startsWith) {
+	    String.prototype.startsWith = function(prefix) {
+	        return this.indexOf(prefix, 0) === 0;
+	    };
+	}
+
+	if (!Date.prototype.addDays) {
+	    Date.prototype.addDays = function(daysCount) {
+	        var dat = new Date(this.valueOf());
+	        dat.setDate(dat.getDate() + daysCount);
+	        return dat;
+	    }
+	}
+
+	Function.prototype.memoized = function () {
+	    var key = JSON.stringify(Array.prototype.slice.call(arguments));
+	    this._values = this._values || {};
+	    return this._values[key] !== undefined ?
+	        this._values[key] :
+	        this._values[key] = this.apply(this, arguments);
+	};
+
+	Function.prototype.memoize = function () {
+	    var fn = this;
+	    return function () {
+	        return fn.memoized.apply(fn, arguments);
+	    };
+	};
+
+	var Colibri = {};
+	Colibri.utils = {};
+
+	Colibri.utils.createGuid = (function () {
+	    function s4() {
+	        return Math.floor((1 + Math.random()) * 0x10000)
+	                   .toString(16)
+	                   .substring(1);
+	    }
+	    return function () {
+	        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+	               s4() + '-' + s4() + s4() + s4();
+	    };
+	})();
+
+
+	Colibri.utils.getRandomInt = function(min, max) {
+	    return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+	
+})();
---
+++ tests/spec/array-spec.js
@@ -0,0 +1,150 @@
+describe("Array 'last'", function() {
+	it("is a function of 'Array' prototype", function() {				
+		var actual = Array.prototype.last instanceof Function;
+		expect(actual).toBe(true);
+    });	
+	
+	it("is a function of '[]'", function() {				
+		var actual = [].last instanceof Function;
+		expect(actual).toBe(true);
+    });	
+	
+    it("returns the last element from array with more than one element", function() {
+		var lastElement = 3;
+		var array = [1, 2, lastElement];
+		
+		var actual = array.last();
+		
+        expect(actual).toEqual(lastElement);
+    });
+	
+	it("returns the last element from array with only one element", function() {
+		var lastElement = 3;
+		var array = [lastElement];
+		
+		var actual = array.last();
+		
+        expect(actual).toEqual(lastElement);
+    });
+	
+	it("returns undefined from empty array", function() {		
+		var array = [];
+		
+		var actual = array.last();
+		
+		expect(actual).toBeUndefined();        
+    });	
+});
+
+describe("Array 'first'", function() {
+	it("is a function of 'Array' prototype", function() {				
+		var actual = Array.prototype.first instanceof Function;
+		expect(actual).toBe(true);
+    });	
+	
+	it("is a function of '[]'", function() {				
+		var actual = [].first instanceof Function;
+		expect(actual).toBe(true);
+    });	
+	
+    it("returns the first element from array with more than one element", function() {
+		var firstElement = '12';
+		var array = [firstElement, 2, 3];
+		
+		var actual = array.first();
+		
+        expect(actual).toEqual(firstElement);
+    });
+	
+	it("returns the first element from array with only one element", function() {
+		var firstElement = 3;
+		var array = [firstElement];
+		
+		var actual = array.first();
+		
+        expect(actual).toEqual(firstElement);
+    });
+	
+	it("returns undefined from empty array", function() {		
+		var array = [];
+		
+		var actual = array.first();
+		
+		expect(actual).toBeUndefined();        
+    });	
+});
+
+describe("Array 'isEmpty'", function() {
+	it("is a function of 'Array' prototype", function() {				
+		var actual = Array.prototype.isEmpty instanceof Function;
+		expect(actual).toBe(true);
+    });	
+	
+	it("is a function of '[]'", function() {				
+		var actual = [].isEmpty instanceof Function;
+		expect(actual).toBe(true);
+    });	
+	
+    it("returns false for array with more than one element", function() {		
+		var array = ['er', 2, 3];
+		var actual = array.isEmpty();
+		
+		expect(actual).toBe(false);
+    });
+	
+	it("returns false for array with only one element", function() {
+		var array = ['f'];
+		
+		var actual = array.isEmpty();
+		
+		expect(actual).toBe(false);
+    });
+	
+	it("returns true for empty array", function() {		
+		var array = [];
+		
+		var actual = array.isEmpty();
+		
+		expect(actual).toBe(true);       
+    });	
+});
+
+describe("Array 'contains'", function() {
+	it("is a function of 'Array' prototype", function() {				
+		var actual = Array.prototype.contains instanceof Function;
+		expect(actual).toBe(true);
+    });	
+	
+	it("is a function of '[]'", function() {				
+		var actual = [].contains instanceof Function;
+		expect(actual).toBe(true);
+    });	
+	
+    it("returns true for existing int element", function() {		
+		var array = [2, 0, ''];
+		var actual = array.contains(2);
+		
+		expect(actual).toBe(true);
+    });	
+
+    it("returns true for existing empty string element", function() {		
+		var array = [2, 0, ''];
+		var actual = array.contains('');
+		
+		expect(actual).toBe(true);
+    });	
+
+    it("returns false for not existing element", function() {		
+		var array = [2, 0, ''];
+		var actual = array.contains(1);
+		
+		expect(actual).toBe(false);
+    });
+
+    it("returns false for empty array", function() {		
+		var array = [];
+		var actual = array.contains(1);
+		
+		expect(actual).toBe(false);
+    });
+});
